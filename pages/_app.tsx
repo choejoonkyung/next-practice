e87c1d0e6e5ef6dc4cb5797ee7b3d52eba214910
choejoonkyung <choejunkyung@gmail.com>
2022-02-10 21:40:03 +0900

    feat: portal provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app";
+import { PortalProvider } from "../src/providers/Portal";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PortalProvider>
+      <Component {...pageProps} />
+    </PortalProvider>
+  );
 }
 
 export default MyApp;
